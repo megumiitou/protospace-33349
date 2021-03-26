9150b3d83e850da48d06c7ed5a1ba88d6b6f41d8
megumiitou <78079814+megumiitou@users.noreply.github.com>
2021-03-26 16:49:53 +0900

    showアクションとルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "prototypes#index"
 
-  resource :prototypes, only: [:index, :new, :create]
+  resource :prototypes, only: [:index, :new, :create, :show]
   
 end
